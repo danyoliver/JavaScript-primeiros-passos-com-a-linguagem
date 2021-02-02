ff2648c55ae9a3e55c578329e3961db522ab08d0
dany <danybatista046@gmail.com>
2021-02-02 17:30:30 -0300

    aula 03

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,19 @@
+console.log("Trabalhando com  atribuição de variáveis");
+
+const primeiroNome = "Danielle";
+const sobrenome = "Oliveira";
+
+//console.log(nome + " " + sobrenome);
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+let contador = 0;
+contador = contador + 1;
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idade; // declarando variável 
+idade = 23; // atribuindo valor
+idade = idade + 1;
+console.log(idade);
